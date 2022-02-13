c58df75007f7acf7468052650469a175500e9018
Natalya Medvedeva <natal.medv@mail.ru>
2022-02-13 13:01:15 +0300

    first

---
+++ src/main/java/ru/vtb/opera/OperaApplication.java
@@ -0,0 +1,13 @@
+package ru.vtb.opera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vtb/opera/OperaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vtb.opera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
